c213add1f764c7f987fdb7529aa2d317f3027b82
Auralcat <ashleyzt@hi2.in>
2018-03-16 18:59:24 -0300

    Fix style errors in desafio-semana-2.js

--- desafio-semana-2.js
+++ desafio-semana-2.js
@@ -37,3 +37,21 @@ console.log(somaEImprime(foo, 33));
 /*
   "O valor da variável agora é 33"
 */
+
+/* Crie uma função com as seguintes características
+1. A função deve receber 3 argumentos
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a
+   função deve retornar a string:
+   Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos,
+   somando 2 ao resultado da multiplicação.
+*/
+const fillMeUp = (x, y, z) => {
+    console.log(`x = ${x}, y = ${y}, z = ${z}`);
+    if ([x, y, z].some(t => t == "undefined")) {
+        return "Preencha todos os valores corretamente!";
+    }
+    return [x, y, z].reduce((x, y) => x * y) + 2;
+};
+
+console.log(fillMeUp(1, 2));
