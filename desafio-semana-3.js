ed4ab0e726782a039091f40ecbc5b654042f7692
Auralcat <ashleyzt@hi2.in>
2018-03-17 11:04:21 -0300

    Add more comments and objects

--- desafio-semana-3.js
+++ desafio-semana-3.js
@@ -21,12 +21,70 @@ let pessoa = (nome, sobrenome, genero, idade, altura, peso, andando,
     this.idade = idade;
     this.altura = altura;
     this.peso = peso;
-    this.andando = andando;
-    this.caminhouQuantosMetros = caminhouQuantosMetros;
+    this.andando = false;
+    this.caminhouQuantosMetros = "zero";
 };
 
-//-------------------------------------------------------------------------------
-// Usando composição de objetos para deixar essa função com menos
+// ------------------------------------------------------------------------------
+// EXTRA - Usando composição de objetos para deixar essa função com menos
 // argumentos. Uma boa prática é fazer a função receber 2 argumentos no
 // geral e SE EXTREMAMENTE NECESSÁRIO, 3 argumentos.
-//-------------------------------------------------------------------------------
+// Vamos usar composição de objetos para simplificar esse objeto
+// enorme agora há pouco.
+let oiEuSou = (primeiro_nome, sobrenome, genero) => {
+    this.primeiro_nome = primeiro_nome;
+    this.sobrenome = sobrenome;
+    this.genero = genero;
+};
+
+// ------------------------------------------------------------------------------
+
+// Adicione um método ao objeto 'pessoa' chamado 'fazerAniversario'. O
+// método deve alterar o valor da propriedade 'idade' dessa pessoa,
+// somando '1' a cada vez que for chamado.
+pessoa.fazerAniversario = () => {
+    this.idade++;
+};
+
+/*
+ Adicione um método ao objeto 'pessoa' chamado 'andar', que terá as
+ seguitnes características:
+ - Esse método deve receber por parâmetro um valor que representará a
+ quantidade de metros caminhados;
+ - Ele deve alterar o valor da propriedade 'caminhouQuantosMetros',
+ somando ao valor dessa propriedade a quantidade passada por
+ parâmetro;
+ - Ele deverá modificar o valor da propriedade 'andando' para o valor
+ booleano que representa "verdadeiro";
+ */
+pessoa.andar = (metrosCaminhados) => {
+    this.caminhouQuantosMetros += metrosCaminhados;
+    this.andando = true;
+};
+
+/*
+ Adicione um método ao objeto 'pessoa' chamado 'parar', que irá
+ modificar o valor da propriedade 'andando' para o valor booleano que
+ representa "falso".
+ */
+pessoa.parar = () => {
+    this.andando = false;
+};
+
+/*
+ Crie um método chamado 'nomeCompleto', que retorne a frase:
+ - "Olá! Meu nome é [NOME] [SOBRENOME]!"
+ */
+pessoa.nomeCompleto = () => `Olá! Meu nome é ${this.nome} ${this.sobrenome}!`;
+
+/*
+ Crie um método chamado 'mostrarIdade', que retorne a frase:
+ - "Olá, eu tenho [IDADE] anos!"
+ */
+pessoa.mostrarIdade = () => `Olá! eu tenho ${this.idade} anos!`;
+
+/*
+ Crie um método chamado 'mostrarPeso', que retorne a frase:
+ - "Eu peso [PESO] Kg."
+ */
+pessoa.mostrarPeso = () => `Eu peso ${this.peso} Kg.`;
