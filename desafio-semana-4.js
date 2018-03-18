7caeebaffc6d64e037c0ed7d73072bd4cc3561c9
Auralcat <ashleyzt@hi2.in>
2018-03-18 14:25:35 -0300

    Create criarCarro constructor and carro object

--- desafio-semana-4.js
+++ desafio-semana-4.js
@@ -36,7 +36,38 @@ for (let i in truthyValues) {
  - 'placa' - String
  - 'ano' - Number
  - 'cor' - String
- - 'quanntasPortas' - Number
+ - 'quantasPortas' - Number
  - 'assentos' - Number - cinco por padrão
  - 'quantidadePessoas' - Number - zero por padrão
+ */
+
+// Montar construtor
+const criarCarro = (marca, modelo, placa, ano, cor, quantasPortas) => {
+    return {
+        // ES6 properties
+        marca, modelo, placa,
+        ano, cor, quantasPortas,
+
+        assentos: 5,
+        quantidadePessoas: 0
+    };
+};
+
+let carro = criarCarro("Hafei", "Towner", "ABC-1234",
+                       1997, "azul-bebê", 4);
+/*
+ Crie um método chamado 'mudarCor', que mude a cor do carro conforme a
+ cor passada por parâmetro.
+ */
+
+/*
+ Crie um método chamado 'obterCor', que retorne a cor do carro.
+ */
+
+/*
+ Crie um método chamado 'obterModelo', que retorne o modelo do carro.
+ */
+
+/*
+ Crie um método chamado 'obterMarca', que retorne a marca do carro.
  */
