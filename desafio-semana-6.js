720456462f5e161f5fce19f06ec68ee17d9a54f2
Auralcat <ashleyzt@hi2.in>
2018-03-22 13:47:43 -0300

    Create tabelaCores

--- desafio-semana-6.js
+++ desafio-semana-6.js
@@ -51,9 +51,54 @@ const showTeamPosition = pos => {
     return null;
 };
 
-console.log(showTeamPosition(0));
-
 module.exports = {
     championship, teams,
     showTeamPosition,
+};
+
+/*
+ Escolha 4 times do campeonato selecionado e mostre a posição dele,
+ usando a função acima. Entre esses 4, adicione 1 que não esteja entre
+ os 5 primeiros.
+ */
+const indiceTimes = [1, 3, 2, 6];
+for (num in indiceTimes) {
+    console.log(showTeamPosition(indiceTimes[num]));
+}
+
+/*
+ Mostre os números de 20 a 30 no console (inclusive o 30), usando a
+ estrutura de repetição "while".
+ */
+let i = 20;
+while (i <= 30) {
+    console.log(i);
+    i++;
+}
+/*
+ Crie uma função chamada 'convertToHex', com as seguintes
+ características:
+ - A função recebe uma cor por parâmetro, do tipo string. Exemplo:
+ "red";
+ - Escolha 5 cores que serão convertidas do nome da cor para o seu
+ equivalente hexadecimal (pode ser qualquer tom);
+ - Usando a estrutura switch, verifique se a cor passada por parâmetro
+ é algum hexa escolhido. Se for, retorne a frase:
+ "O hexadecimal para a cor [COR] é [HEXADECIMAL].";
+ - Se a cor passada por parâmetro nõa estiver entre as selecionadas,
+ mostre a frase:
+ "Não temos o equivalente hexadecimal para [COR]."
+ */
+
+// Vamos precisar de uma tabela de cores:
+const tabelaCores = {
+    red: "#ff0000",
+    blue: "#0000ff",
+    green: "#00ff00",
+    black: "#000000",
+    white: "#ffffff",
+    aquamarine: "#7fffd4",
+    yellow: "#ffff00",
+    magenta: "#ff00ff",
+    cyan: "#00ffff"
 };
