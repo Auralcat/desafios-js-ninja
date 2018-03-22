86ac8263ecbb6f932683f1cf3605408b34d57f45
Auralcat <ashleyzt@hi2.in>
2018-03-22 11:45:16 -0300

    Create test file for challenge 06

---
+++ test/desafio-semana-6-test.js
@@ -0,0 +1,13 @@
+// Apontar o teste pro arquivo que você quer
+let desafio06 = require('../desafio-semana-6.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// Importar funções e variáveis que vamos testar
+// Funções
+
+// Variáveis
+
+describe("Teste desafio 06", function() {
+
+});
