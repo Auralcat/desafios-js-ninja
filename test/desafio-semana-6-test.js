b78af70f44c882e8c8b75dcd65f323fa42bd9e1b
Auralcat <ashleyzt@hi2.in>
2018-03-22 11:51:29 -0300

    Write test for teams variable

--- test/desafio-semana-6-test.js
+++ test/desafio-semana-6-test.js
@@ -8,11 +8,18 @@ let assert = require("assert");
 
 // Variáveis
 let championship = desafio06.championship;
+let teams = desafio06.teams;
 
 describe("Teste desafio 06", function() {
     describe("Variáveils", function() {
         it("championship deve conter um String", function() {
             assert.equal(typeof(championship), "string");
         });
+
+        it("teams deve conter 5 Strings", function() {
+            assert.equal(teams.length, 5);
+            assert.equal(teams.some(x => typeof(x) != "string"), false);
+        });
+
     });
 });
