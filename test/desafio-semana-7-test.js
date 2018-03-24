a90e96f180f82bede456f53954a85d774156b0bb
Auralcat <ashleyzt@hi2.in>
2018-03-24 12:23:28 -0300

    Touch test for challenge 07

---
+++ test/desafio-semana-7-test.js
@@ -0,0 +1,13 @@
+// Apontar o teste pro arquivo que você quer
+let desafio07 = require('../desafio-semana-6.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// Importar funções e variáveis que vamos testar
+// Funções
+
+// Variáveis
+
+describe("Teste desafio 07", function() {
+
+});
