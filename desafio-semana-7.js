4e3fcf94e2d0d5c92213d39503d5892bcc43ef14
Auralcat <ashleyzt@hi2.in>
2018-03-24 13:57:21 -0300

    Create logRange

--- desafio-semana-7.js
+++ desafio-semana-7.js
@@ -33,20 +33,18 @@ console.log(`O primeiro array tem ${arr.length} itens.`);
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O segundo array tem ${blag.length} itens.`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
-// ?
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -54,11 +52,14 @@ Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
+
+// Não quero repetir código
+const logRange = (start, stop, condition) => {
+
+};
 console.log( 'Números pares entre 100 e 120:' );
-// ?
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
 
 module.exports = {
     arr, blag,
