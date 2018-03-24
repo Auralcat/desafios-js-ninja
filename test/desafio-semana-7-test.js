6ddd6d8a34433f1757d4159fb94e3e2c0595715a
Auralcat <ashleyzt@hi2.in>
2018-03-24 13:34:04 -0300

    Add tests for addItem

--- test/desafio-semana-7-test.js
+++ test/desafio-semana-7-test.js
@@ -5,9 +5,31 @@ let assert = require("assert");
 
 // Importar funções e variáveis que vamos testar
 // Funções
+let addItem = desafio07.addItem;
 
 // Variáveis
+let arr = desafio07.arr;
 
 describe("Teste desafio 07", function() {
+    describe("Variáveis", function() {
+        it("arr deve ter 5 itens de tipos diferentes", function(){
+            assert.equal(arr.length, 5);
+            assert.equal(new Set(arr.map(x => typeof(x))).size, arr.length);
+        });
+    });
+
+    describe("Funções", function() {
+        it("addItem deve retornar o array com o novo elemento", function() {
+            let item = "bar";
+            assert.equal(addItem(arr, item).length, arr.length + 1);
+        });
+
+        it("addItem deve permitir adição de arrays no mesmo nível", function() {
+            let blah = ["cupcake", "princess", "sparkly"];
+            let concatArr = arr.concat(blah);
+            let addedArr = addItem(arr, blah);
+            assert.equal(addedArr.length == concatArr.length);
+        });
+    });
 
 });
