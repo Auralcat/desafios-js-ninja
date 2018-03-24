0a2ec8c3518e7ad2fab4c9434a8d4b377c8a3d96
Auralcat <ashleyzt@hi2.in>
2018-03-24 13:46:36 -0300

    Do challenge 07

--- desafio-semana-7.js
+++ desafio-semana-7.js
@@ -1,34 +1,33 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
 let arr = [1, "snazzy", /wild-regex/, undefined, true];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
 const addItem = (arr, item) => arr.concat(item);
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+let blag = [24, /another-regex/, false];
+console.log(addItem(arr, blag));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log(`O segundo elemento do segundo array é ${blag[1]}`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O primeiro array tem ${arr.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
