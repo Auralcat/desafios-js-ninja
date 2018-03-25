a45d6d1a1e14166d0d82af4d61df5283246deb85
Auralcat <ashleyzt@hi2.in>
2018-03-25 13:29:49 -0300

    Write initial tests for challenge 08

--- test/desafio-semana-8-test.js
+++ test/desafio-semana-8-test.js
@@ -14,6 +14,32 @@ describe("Teste desafio 08", function() {
     });
 
     describe("Funções", function() {
+        it("calculateSum deve receber dois números", function() {
+
+        });
+        it("calculateSum deve retornar a soma dos argumentos", function() {
+            assert.equal(calculateSum(2, 4), 6);
+        });
+
+        it("showName deve retornar um String", function() {
+            assert.equal(typeof(showName()), "string");
+        });
+
+        it("calculator deve receber dois números e um operador", function(){
+
+        });
+
+        it("calculator só pode receber como operadores: +, -, *, /, %", function() {
+
+        });
+
+        it("calculator deve retornar o resultado da operação fornecida", function() {
+            assert.equal(calculator(2, 10, "+"), (2 + 10));
+            assert.equal(calculator(10, 5, "-"), (10 - 5));
+            assert.equal(calculator(13, 5, "*"), (13 * 5));
+            assert.equal(calculator(40, 8, "/"), (40 / 8));
+            assert.equal(calculator(30, 2, "%"), (30 % 2));
+        });
 
     });
 
