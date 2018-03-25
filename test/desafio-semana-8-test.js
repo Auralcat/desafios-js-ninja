6ec07592ec29826bd98d969ad7a6a989cce577df
Auralcat <ashleyzt@hi2.in>
2018-03-25 13:21:13 -0300

    Touch challenge and test files for challenge 08

---
+++ test/desafio-semana-8-test.js
@@ -0,0 +1,20 @@
+// Apontar o teste pro arquivo que você quer
+let desafio08 = require('../desafio-semana-8.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// Importar funções e variáveis que vamos testar
+// Funções
+
+// Variáveis
+
+describe("Teste desafio 08", function() {
+    describe("Variáveis", function() {
+
+    });
+
+    describe("Funções", function() {
+
+    });
+
+});
