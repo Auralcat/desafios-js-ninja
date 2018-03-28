c4a38efef6bbcbec3d439cf9e69f71e09ec0f88d
Auralcat <ashleyzt@hi2.in>
2018-03-28 08:32:04 -0300

    Touch files for challenge 11

---
+++ test/desafio-semana-11-test.js
@@ -0,0 +1,12 @@
+// Apontar o teste pro arquivo que você quer
+let desafio11 = require('../desafio-semana-11.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// A importação das variáveis e funções vai ser um pouco diferente
+// nesse caso...
+let iifeInternals = desafio11.iife;
+
+describe("Teste desafio 11", function() {
+
+});
