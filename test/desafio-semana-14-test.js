ab047db5119b4e864996b7d34793b44d2abd425a
Auralcat <ashleyzt@hi2.in>
2018-03-30 09:32:49 -0300

    Touch files for challenge 14

---
+++ test/desafio-semana-14-test.js
@@ -0,0 +1,12 @@
+// Apontar o teste pro arquivo que você quer
+let desafio14 = require('../desafio-semana-14.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// A importação das variáveis e funções vai ser um pouco diferente
+// nesse caso...
+let iifeInternals = desafio14.iife;
+
+describe("Teste desafio 14", function() {
+
+});
