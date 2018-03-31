ae00f1efd0668a94d4fcc35ef3e09fe1f5ba16d2
Auralcat <ashleyzt@hi2.in>
2018-03-31 10:18:33 -0300

    Touch files for challenge 16

---
+++ desafio-16/test/desafio-semana-16-test.js
@@ -0,0 +1,12 @@
+// Apontar o teste pro arquivo que você quer
+let desafio16 = require('../desafio-semana-16.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// A importação das variáveis e funções vai ser um pouco diferente
+// nesse caso...
+let iifeInternals = desafio16.iife;
+
+describe("Teste desafio 16", function() {
+
+});
