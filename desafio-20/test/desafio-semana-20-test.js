267fab794a62c4da4ef0715955cb7aab96b6f6ef
Auralcat <ashleyzt@hi2.in>
2018-04-03 14:23:52 -0300

    Touch files for challenge 20

---
+++ desafio-20/test/desafio-semana-20-test.js
@@ -0,0 +1,11 @@
+// Apontar o teste pro arquivo que você quer
+let desafio20 = require('../desafio-semana-20.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+// A importação das variáveis e funções vai ser um pouco diferente
+// nesse caso...
+let iifeInternals = desafio20.iife;
+
+describe("Teste desafio 20", function() {
+});
