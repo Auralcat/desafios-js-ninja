ff501b709f16d6aded76b55896ec49643ece7c7c
Auralcat <ashleyzt@hi2.in>
2018-04-04 11:34:37 -0300

    Clean test file, add required elements in HTML for challenge

--- desafio-21/test/desafio-semana-21-test.js
+++ desafio-21/test/desafio-semana-21-test.js
@@ -3,10 +3,6 @@ let desafio21 = require('../desafio-semana-21.js');
 // Usando o assert do Node.js
 let assert = require("assert");
 
-// A importação das variáveis e funções vai ser um pouco diferente
-// nesse caso...
-let iifeInternals = desafio21.iife;
-
 describe("Teste desafio 21", function() {
 
 });
