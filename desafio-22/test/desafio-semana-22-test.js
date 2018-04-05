263dba1a9f54a1ee9180f5550f0d0682c996d115
Auralcat <ashleyzt@hi2.in>
2018-04-05 08:33:35 -0300

    Touch files for test 22

---
+++ desafio-22/test/desafio-semana-22-test.js
@@ -0,0 +1,8 @@
+// Apontar o teste pro arquivo que você quer
+let desafio22 = require('../desafio-semana-22.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+describe("Teste desafio 22", function() {
+
+});
