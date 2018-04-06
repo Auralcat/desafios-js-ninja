11da731ab47eb77ea03a4a0c95c5786366af9e57
Auralcat <ashleyzt@hi2.in>
2018-04-06 20:15:43 -0300

    Add Chai assertion library

--- desafio-23/test/desafio-semana-23-test.js
+++ desafio-23/test/desafio-semana-23-test.js
@@ -1,8 +1,9 @@
-// Apontar o teste pro arquivo que você quer
-let desafio23 = require('../desafio-semana-23.js');
-// Usando o assert do Node.js
-let assert = require("assert");
+// We need Chai to do the assertions in the browser.
+// Makes sense since you're working away from Node this time.
+let assert = chai.assert;
 
 describe("Teste desafio 23", function() {
-
+    it("$digit0 contém um objeto", function() {
+        assert.equal(typeof($digit0), "object");
+    });
 });
