7a6cec0d56920ef5fa07224844b3ee477bd9e842
Auralcat <ashleyzt@hi2.in>
2018-04-06 19:41:04 -0300

    Correct number assignment

--- desafio-23/desafio-semana-23.js
+++ desafio-23/desafio-semana-23.js
@@ -28,12 +28,11 @@ input;
 // Primeiro vou fazer um array de objetos do DOM
 let rawDigitSelectors = [ "0", "1", "2", "3",
                           "4", "5", "6", "7", "8", "9" ].map(n => {
-    document.getElementById("digit-" + n);
+    return document.getElementById("digit-" + n);
 });
 
 // Agora vou usar o spread operator pra passar essas variáveis pra outra forma
-let $digit0, $digit1, $digit2, $digit3, $digit4, $digit5,
-    $digit6, $digit7, $digit8, $digit9 = [...rawDigitSelectors];
+let [ $digit0, $digit1, $digit2, $digit3, $digit4, $digit5,
+    $digit6, $digit7, $digit8, $digit9 ] = [...rawDigitSelectors];
 
-console.log($digit0.innerHTML());
 // Operações
