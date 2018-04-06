431ee8aafb9578f0b8c63124aa8de3037e6d2cbb
Auralcat <ashleyzt@hi2.in>
2018-04-06 20:23:27 -0300

    Get ready to write tests, disable input editing

--- desafio-23/test/desafio-semana-23-test.js
+++ desafio-23/test/desafio-semana-23-test.js
@@ -1,9 +1,11 @@
 // We need Chai to do the assertions in the browser.
 // Makes sense since you're working away from Node this time.
+// No need for using module.exports like this. Or require().
 let assert = chai.assert;
 
 describe("Teste desafio 23", function() {
     it("$digit0 contém um objeto", function() {
         assert.equal(typeof($digit0), "object");
     });
+
 });
