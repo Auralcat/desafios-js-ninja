0eba5d154936ba41a03a41e4a1f1fc039b632675
Auralcat <ashleyzt@hi2.in>
2018-04-06 18:38:33 -0300

    Touch files for challenge 23

---
+++ desafio-23/test/desafio-semana-23-test.js
@@ -0,0 +1,8 @@
+// Apontar o teste pro arquivo que você quer
+let desafio23 = require('../desafio-semana-23.js');
+// Usando o assert do Node.js
+let assert = require("assert");
+
+describe("Teste desafio 23", function() {
+
+});
