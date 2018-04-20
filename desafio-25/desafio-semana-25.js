7903f1cca304b0a364ee15fcb3be335813569e24
Auralcat <ashleyzt@hi2.in>
2018-04-20 10:40:41 -0300

    Write event list

--- desafio-25/desafio-semana-25.js
+++ desafio-25/desafio-semana-25.js
@@ -14,3 +14,129 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+/* Lista dos eventos:
+ Recursos:
+ - cached
+ - error
+ - abort
+ - load
+ - beforeunload
+ - unload
+
+ Rede:
+ - online
+ - offline
+
+ Foco:
+ - focus
+ - blur
+
+ Websocket:
+ - open
+ - message
+ - error
+ - closed
+
+ História da Sessão:
+ - pagehide
+ - pageshow
+ - popstate
+
+ Animações CSS:
+ - animationstart
+ - animationend
+ - animationiteration
+
+ Transições CSS:
+ - transitionstart
+ - transitionend
+ - transitioncancel
+ - transitionrun
+
+ Formulários:
+ - reset
+ - submit
+
+ Impressão:
+ - beforeprint
+ - afterprint
+
+ Composição de texto:
+ - compositionstart
+ - compositionupdate
+ - compositionend
+
+ Views:
+ - fullscreenchange
+ - fullscreenerror
+ - resize
+ - scroll
+
+ Área de transferência;
+ - cut
+ - copy
+ - paste
+
+ Teclado:
+ - keyup
+ - keydown
+ - keypress
+
+ Mouse:
+ - mouseenter
+ - mouseover
+ - mousemove
+ - mousedown
+ - mouseup
+ - auxclick
+ - click
+ - dblclick
+ - contextmenu
+ - wheel
+ - mouseleave
+ - mouseout
+ - select
+ - pointerlockchange
+ - pointerlockerror
+
+ Drag e drop:
+ - dragstart
+ - drag
+ - dragend
+ - dragenter
+ - dragover
+ - dragleave
+ - drop
+
+ Mídia:
+ - durationchange
+ - loadedmetadata
+ - loadeddata
+ - canplay
+ - canplaythrough
+ - ended
+ - emptied
+ - stalled
+ - suspended
+ - play
+ - playing
+ - pause
+ - waiting
+ - seeking
+ - seeked
+ - ratechange
+ - timeupdate
+ - volumechange
+ - complete
+ - audioprocess
+
+ Progresso:
+ - loadstart
+ - progress
+ - error
+ - timeout
+ - abort
+ - load
+ - loadend
+ */
